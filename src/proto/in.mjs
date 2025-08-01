df0cb071abc4eda17624933f8926a49c8cb5c62d
𝖊𝖈𝖍𝖔 <littel.coding@gmail.com>
2025-08-01 17:14:08 +0300

    feat(in): support substring checks in strings

--- src/proto/in.mjs
+++ src/proto/in.mjs
@@ -1,8 +1,8 @@
 import is from '../core.mjs';
 
 /**
- * Determines if `is.prototype.actual` in `object` or `array`,
- * with optional strict check for ownProperties
+ * Determines if `is.prototype.actual` in `object`, `array` or `string`,
+ * with optional strict check for `object` ownProperties
  *
  * @param {*} object - Object to search in
  * @param {object} [options] - Optional configration object
@@ -11,22 +11,25 @@ import is from '../core.mjs';
  * @returns {boolean}
  *
  * @example
- * const a = {one: 1};
- * const b = Object.create(a);
- * b.tow = 2;
+ * const str = 'a b c';
+ * const arr = [1, 'a', true];
+ * const abj1 = {one: 1};
+ * const obj2 = Object.create(obj1);
+ * obj2.tow = 2;
  *
- * is('a').in([1, 'a', true]) // true
- * is('one').in(b) // true
- * is('tow').in(b) // true
- * is('one').in(b, {mode: 'own'}) // false
- * is('tow').in(b, {mode: 'own'}) // true
+ * is('b').in(str) // true
+ * is('a').in(arr) // true
+ * is('one').in(obj2) // true
+ * is('tow').in(obj2) // true
+ * is('one').in(obj2, {mode: 'own'}) // false
+ * is('tow').in(obj2, {mode: 'own'}) // true
  *
  * @memberof is
  * @instance
  * @since 1.0.0
  */
 is.prototype.in = function (object, options = { mode: 'all' }) {
-  if (is.array(object)) {
+  if (typeof object === 'string' || is.array(object)) {
     return object.includes(this.actual);
   } else if (is.object(object)) {
     if (options.mode === 'own') {

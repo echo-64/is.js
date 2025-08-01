912696b637f176b115cd09136d8ed67011c353e7
𝖊𝖈𝖍𝖔 <littel.coding@gmail.com>
2025-08-01 17:23:33 +0300

    feat(object): add option to control empty-object matching

--- src/proto/object.mjs
+++ src/proto/object.mjs
@@ -3,11 +3,13 @@ import json5 from 'json5';
 
 /**
  * Checks if `is.prototype.actual` is object,
- * with optional behavior for string object
+ * with optional behavior for string object and empty ones
  *
  * @param {object} [options] - Optional configration object
- * @param {boolean} [options.string] - If true, also considers the string object "{...}"
- * as valid object matches
+ * @param {boolean} [options.string] - If true, also considers
+ * the string object "{...}" as valid object matches
+ * @param {boolean} [options.empty] - If false, considers
+ * empty object `{}` as invalid object matches
  *
  * @returns {boolean}
  *
@@ -15,19 +17,27 @@ import json5 from 'json5';
  * is({a: 1}).object() // true
  * is('{a: 1}').object({string: true}) // true
  * is('{"a": "1"}').object({string: true}) // true
+ * is({}).object({empty: false}) // false
  *
  * @memberof is
  * @instance
  * @since 1.0.0
  */
-is.prototype.object = function (options) {
+is.prototype.object = function (options = {}) {
+  options = {
+    string: options.string ?? false,
+    empty: options.empty ?? true,
+  };
+
   try {
     const parsed =
       options?.string === true && this.string()
         ? json5.parse(this.actual)
         : this.actual;
 
-    return is.object(parsed);
+    return options.empty === true
+      ? is.object(parsed)
+      : is.object(parsed) && Object.keys(parsed).length > 0;
   } catch {
     return false;
   }
